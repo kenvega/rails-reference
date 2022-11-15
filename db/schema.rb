c3b2c4e7d804027505281c97a209798b404e9df6
Ken Vega <ken.vega.g@gmail.com>
2022-11-15 07:12:24 -0500

    add customer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_15_041956) do
+ActiveRecord::Schema.define(version: 2022_11_15_120412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,18 @@ ActiveRecord::Schema.define(version: 2022_11_15_041956) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_books_on_author_id"
     t.index ["supplier_id"], name: "index_books_on_supplier_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name", null: false
+    t.string "title"
+    t.string "email", null: false
+    t.integer "visits", default: 0, null: false
+    t.integer "orders_count", default: 0, null: false
+    t.integer "lock_version"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "suppliers", force: :cascade do |t|
